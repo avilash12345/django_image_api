5be5045c37672b6986b463e095bbd1fbfb52b9cb
avilash <65917508+avilash12345@users.noreply.github.com>
2022-04-17 17:39:31 +0530

    Delete api directory

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from rest_framework import routers
-from api.views import CategoryViewSet, ImageViewSet
-router = routers.DefaultRouter()
-router.register(r'Category', CategoryViewSet)
-router.register(r'Image', ImageViewSet)
-urlpatterns = [
-   path('', include(router.urls)),
-]
